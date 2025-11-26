681cb5246d80c65a648c6f8bdb4ae693d0795fc6
Ben Severns <severns3@gmail.com>
2025-11-26 08:39:18 -0600

    Improve TinyUSB MIDI shim transmit routing

--- firmware/platformio/lib/MIDIUSB/MIDIUSB.h
+++ firmware/platformio/lib/MIDIUSB/MIDIUSB.h
@@ -7,6 +7,7 @@
 // the TinyUSB-backed Adafruit_USBD_MIDI instance you define in your sketch.
 
 #include <Adafruit_TinyUSB.h>
+#include <cstring>
 #include <type_traits>
 #include <utility>
 
@@ -30,6 +31,52 @@ struct has_send_midi<Midi, std::void_t<decltype(
                                std::declval<Midi &>().send(
                                    std::declval<const midiEventPacket_t &>()))>>
     : std::true_type {};
+
+template <typename Midi, typename = void>
+struct has_send_midi_packet : std::false_type {};
+
+template <typename Midi>
+struct has_send_midi_packet<
+    Midi, std::void_t<decltype(std::declval<Midi &>().sendMIDI(
+        std::declval<const midiEventPacket_t &>()))>> : std::true_type {};
+
+template <typename Midi, typename = void>
+struct has_send_packet_ptr : std::false_type {};
+
+template <typename Midi>
+struct has_send_packet_ptr<
+    Midi, std::void_t<decltype(std::declval<Midi &>().send(
+        std::declval<const midiEventPacket_t *>()))>> : std::true_type {};
+
+template <typename Midi, typename = void>
+struct has_send_packet_call : std::false_type {};
+
+template <typename Midi>
+struct has_send_packet_call<
+    Midi, std::void_t<decltype(std::declval<Midi &>().sendPacket(
+        std::declval<const midiEventPacket_t &>()))>> : std::true_type {};
+
+template <typename Midi, typename = void>
+struct has_write_packet : std::false_type {};
+
+template <typename Midi>
+struct has_write_packet<Midi, std::void_t<decltype(
+                               std::declval<Midi &>().write(
+                                   reinterpret_cast<const uint8_t *>(
+                                       std::declval<const midiEventPacket_t *>()),
+                                   sizeof(midiEventPacket_t)))>> : std::true_type {};
+
+template <typename Midi>
+struct has_any_tx
+    : std::integral_constant<
+          bool, has_send_midi<Midi>::value ||
+                    has_send_midi_packet<Midi>::value ||
+                    has_send_packet_ptr<Midi>::value ||
+                    has_send_packet_call<Midi>::value ||
+                    has_write_packet<Midi>::value> {};
+
+template <int I> struct priority_tag : priority_tag<I - 1> {};
+template <> struct priority_tag<-1> {};
 } // namespace detail
 
 class MIDIUSB_t {
@@ -40,16 +87,11 @@ public:
   midiEventPacket_t read(void) { return adaptPacket(usb_midi.read()); }
 
   void sendMIDI(const midiEventPacket_t &event) {
-    // Adafruit TinyUSB Library v3.x exposes Adafruit_USBD_MIDI::send(packet).
-    // This firmware targets that API. If a future TinyUSB drop renames the
-    // transmit hook again, fail fast at compile time instead of silently
-    // discarding outbound MIDI traffic.
-    static_assert(
-        detail::has_send_midi<Adafruit_USBD_MIDI>::value,
-        "Adafruit_USBD_MIDI::send(midiEventPacket_t) missing; update the "
-        "shim if TinyUSB changes its transmit API.");
+    static_assert(detail::has_any_tx<Adafruit_USBD_MIDI>::value,
+                  "Adafruit_USBD_MIDI needs a send-like routine; update the "
+                  "shim to match the library's transmit API.");
 
-    (void)usb_midi.send(event);
+    forwardSend(usb_midi, event, detail::priority_tag<4>{});
   }
 
   void flush(void) { usb_midi.flush(); }
@@ -73,18 +115,35 @@ private:
 
   template <typename Midi>
   static auto forwardSend(Midi &midi, const midiEventPacket_t &event,
-                          int) -> decltype(midi.sendMIDI(event), void()) {
+                          detail::priority_tag<4>)
+      -> decltype(midi.sendMIDI(event), void()) {
     midi.sendMIDI(event);
   }
 
   template <typename Midi>
   static auto forwardSend(Midi &midi, const midiEventPacket_t &event,
-                          long) -> decltype(midi.send(event), void()) {
+                          detail::priority_tag<3>)
+      -> decltype(midi.send(event), void()) {
     midi.send(event);
   }
 
   template <typename Midi>
-  static auto forwardSend(Midi &midi, const midiEventPacket_t &event, char)
+  static auto forwardSend(Midi &midi, const midiEventPacket_t &event,
+                          detail::priority_tag<2>)
+      -> decltype(midi.send(&event), void()) {
+    midi.send(&event);
+  }
+
+  template <typename Midi>
+  static auto forwardSend(Midi &midi, const midiEventPacket_t &event,
+                          detail::priority_tag<1>)
+      -> decltype(midi.sendPacket(event), void()) {
+    midi.sendPacket(event);
+  }
+
+  template <typename Midi>
+  static auto forwardSend(Midi &midi, const midiEventPacket_t &event,
+                          detail::priority_tag<0>)
       -> decltype(midi.write(reinterpret_cast<const uint8_t *>(&event),
                              sizeof(event)),
                   void()) {
@@ -92,7 +151,8 @@ private:
   }
 
   template <typename Midi>
-  static void forwardSend(Midi &, const midiEventPacket_t &, ...) {
+  static void forwardSend(Midi &, const midiEventPacket_t &,
+                          detail::priority_tag<-1>) {
 #if defined(ARDUINO)
 #warning "No Adafruit_USBD_MIDI send routine detected; MIDIUSB shim sendMIDI() will be a no-op."
 #endif
